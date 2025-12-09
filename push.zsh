aff8b40736cea6f86c2e76cb204e53c115fd64fa
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2025-12-09 12:07:37 +0100

    empty start

--- push.zsh
+++ push.zsh
@@ -1,4 +1,4 @@
 git add -A && \
 git commit -m "$1" && \
 git push && \
-git push 01-ser kill-ux
+git push webserver origin
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,4 @@
-use server::config::*;
 
 fn main() {
-    let config = match Config::parse() {
-        Ok(cfg) => cfg,
-        Err(err) => {
-            println!("Error : {}", err);
-            return;
-        }
-    };
-    dbg!(config);
-
-    for server in config.servers {
-        
-    }
-    
+   
 }
